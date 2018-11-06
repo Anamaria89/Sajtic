7e049a32e604859b9eb4ed351e55d6d78a37a0ca
Anamaria <amic_dragic@yahoo.com>
2018-11-06 12:41:52 +0100

    MVC PHP

---
+++ sajtic/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "sajtic");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/sajtic/");
+define("ROOT_URL", "http://sajtic/sajtic/");
